d5e9ab3ac43ba6837ef27cf668d3edd012ae4ce6
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-02 16:21:10 +0600

    basic types of typescript

---
+++ module1/source/1.5.ts
@@ -0,0 +1,18 @@
+// Reference type --> Object
+
+const user: {
+  company: "Programming Hero"; // this value acts a type. such are called literal type
+  readonly firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "Programming Hero",
+  firstName: "Farah",
+  //   middleName: "Samsi",
+  lastName: "Prome",
+  isMarried: true,
+  //   isMarried: 56,
+};
+
+user.firstName = "farah"; //readonly property abar reassign kora jabe na
